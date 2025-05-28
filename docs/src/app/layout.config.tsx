d170838bd9782c52a0ebc5cf22c3e1fce082f9d9
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-05-28 16:07:09 -0400

    feat: scaffold api section

--- docs/src/app/layout.config.tsx
+++ docs/src/app/layout.config.tsx
@@ -1,5 +1,5 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
-import { BookTextIcon, ShapesIcon } from "lucide-react";
+import { BookTextIcon, CodeXmlIcon, ShapesIcon } from "lucide-react";
 import Image from "next/image";
 
 import icon from "@@/public/icon-black.svg";
@@ -27,6 +27,12 @@ export const baseOptions: BaseLayoutProps = {
       url: "/docs",
       active: "nested-url",
       icon: <BookTextIcon />,
+    },
+    {
+      text: "API Reference",
+      url: "/api",
+      active: "nested-url",
+      icon: <CodeXmlIcon />,
     },
     {
       text: "Guides",
