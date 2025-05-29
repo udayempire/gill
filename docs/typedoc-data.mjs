1a28ba1b3daf6fefb4d234628b004eda49bda5da
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-05-29 12:50:46 -0400

    feat: api docs

---
+++ docs/typedoc-data.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+
+/**
+ * Declare data and information to be injected into the
+ * TypeDoc generated API reference's index files
+ */
+export const typedocPageIndexData = {
+  gill: {
+    frontmatter: {
+      title: "gill",
+      description:
+        "A modern JavaScript/TypeScript client library for interacting with the Solana blockchain.",
+    },
+  },
+  "gill-node": {
+    frontmatter: {
+      title: "gill/node",
+      description:
+        "Functions and utilities designed for interacting with the Solana blockchain using JavaScript server backends and runtimes.",
+    },
+  },
+  react: {
+    frontmatter: {
+      title: "gill-react",
+      description:
+        "A React hooks library for easily interacting with the Solana blockchain. Built on top of gill and TanStack Query.",
+    },
+  },
+};
